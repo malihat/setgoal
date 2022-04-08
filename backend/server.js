3975be963f468708c8e48dee0abdd506abb90d85
Maliha Tanweer <malihatanweer@gmail.com>
2022-04-08 16:31:26 -0400

    Initial REST Api for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
 const goalRoutes = require('./routes/goalRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware')
+
+connectDB();
 
 const app = express();
 app.use(express.json());
